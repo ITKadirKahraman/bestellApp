47a07129514565e0ceaa178f1cf5c0a847f18241
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-07-17 13:18:34 +0200

    unit test

---
+++ tests/berechneGesamtpreis.test.js
@@ -0,0 +1,17 @@
+import test from "node:test";
+import assert from "node:assert/strict";
+import { berechneGesamtpreis } from "./berechneGesamtpreis.js"
+
+test(("calculate the price from order"), () => {
+    const warenkorb = [
+        {name: "Pizza", price: 14.99, amount: 1},
+        {name: "Coffee", price: 4.99, amount: 1},
+    ]
+    assert.equal(berechneGesamtpreis(warenkorb), 19.98)
+})
+
+test.only(("product amount"), () => {
+    const warenkorb = [
+        {name: "Döner", price: 8.50, amount: 4}
+    ]
+})
