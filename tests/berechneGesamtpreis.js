e56c11713a47cbd179ca852f295ddeb47e5e17f7
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-07-17 22:54:14 +0200

    fixed funktion

--- tests/berechneGesamtpreis.js
+++ tests/berechneGesamtpreis.js
@@ -1,7 +1,7 @@
 export function berechneGesamtpreis(artikel) {
     let summe = 0;
     for(const a of artikel) {
-        summe = summe + a.price;
+        summe = summe + (a.price * a.amount);
     }
 
     return summe;
