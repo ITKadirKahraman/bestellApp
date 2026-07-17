a854010506187fa9150a6c81fc09f9f69dcb800c
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-07-17 13:17:28 +0200

    generate new test

---
+++ tests/array.js
@@ -0,0 +1,11 @@
+export function testArray(array) {
+    return array = [
+        {
+            image: array[0].image,
+            name: array[0].name,
+            price: array[0].price,
+            description: array[0].description,
+            added: array[0].added,
+        }
+    ]
+}
