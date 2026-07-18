27f5da6610dc4b8bb6917fd09263aae3a1fdd01c
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-07-18 23:44:35 +0200

    added a new unit test for empty basket

--- tests/berechneGesamtpreis.test.js
+++ tests/berechneGesamtpreis.test.js
@@ -7,14 +7,16 @@ test(("calculate the price from order"), () => {
         {name: "Pizza", price: 14.99, amount: 1},
         {name: "Coffee", price: 4.99, amount: 1},
     ]
-    assert.equal(berechneGesamtpreis(warenkorb), 19.98)
+    assert.equal(berechneGesamtpreis(warenkorb), 19.98);
+    console.log(berechneGesamtpreis(warenkorb));
 })
 
-test.only(("product amount"), () => {
+test(("product amount"), () => {
     const warenkorb = [
         {name: "Döner", price: 8, amount: 4}
     ]
-    assert.equal(berechneGesamtpreis(warenkorb), 32)
+    assert.equal(berechneGesamtpreis(warenkorb), 32);
+    console.log(berechneGesamtpreis(warenkorb));
 })
 
 test("beruecksichtigt die Menge (3x der gleiche Artikel)", () => {
@@ -22,4 +24,10 @@ test("beruecksichtigt die Menge (3x der gleiche Artikel)", () => {
     { name: "Döner", price: 20, amount: 3},
   ];
   assert.equal(berechneGesamtpreis(warenkorb), 60);
+  console.log(berechneGesamtpreis(warenkorb));
+});
+
+test.only("empty basket is zero", () => {
+  assert.equal(berechneGesamtpreis([]), 0);
+  console.log(berechneGesamtpreis([]));
 });
