6a552cc1a3d68c35b5f5e7c8c0b13f5fc30454e0
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-07-19 00:12:07 +0200

    unit tests

--- tests/pushToBasket.test.js
+++ tests/pushToBasket.test.js
@@ -2,7 +2,7 @@ import test from "node:test";
 import assert from "node:assert/strict";
 import { pushToBasket } from "./pushToBasket.js";
 
-test.only(("add products to basket"), () => {
+test(("add products to basket"), () => {
     const basket = [];
 
     const product = {
@@ -14,4 +14,25 @@ test.only(("add products to basket"), () => {
     pushToBasket(basket, product);
 
     assert.deepStrictEqual(basket, [{name: "Pizza Döner with Hollandiase", price: 13.50, amount: 1}]);
+    console.log(basket);
+    
+})
+
+test.only(("same product increases the quantity"), () => {
+    const basket = [
+        {
+            name: "Pizza Döner with Hollandiase",
+            price: 13.50,
+            amount: 1
+        }
+    ];
+
+    pushToBasket(basket, {
+        name: "Pizza Döner with Hollandiase",
+        price: 13.50
+    });
+
+    assert.equal(basket.length, 1);
+    assert.equal(basket[0].amount, 2);
+    console.log(basket);
 })
