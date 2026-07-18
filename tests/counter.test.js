c48582ab48308df22e5a22d674b67bd5ad64d37f
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-07-18 23:28:12 +0200

    added two units tests

--- tests/counter.test.js
+++ tests/counter.test.js
@@ -1,8 +1,8 @@
 import test from "node:test";
 import assert from "node:assert/strict";
-import { counterTesting } from "./counter.js"
+import { updateBasketAmount } from "./counter.js"
 
-test.only(("erhöht die Menge eines Produkts"), () => {
+test(("erhöht die Menge eines Produkts"), () => {
     const basket = [
         {
             name: "Pizza",
@@ -11,6 +11,35 @@ test.only(("erhöht die Menge eines Produkts"), () => {
         }
     ];
 
-    counterTesting(basket, 0, 2);
+    updateBasketAmount(basket, 0, 2);
     assert.equal(basket[0].amount, 3);
+    console.log(basket)
+})
+
+test(("verringert die Menge"), () => {
+    const basket = [
+        {
+            name: "Pizza",
+            price: 10,
+            amount: 3
+        }
+    ];
+
+    updateBasketAmount(basket, 0, -2);
+    assert.equal(basket[0].amount, 1);
+    console.log(basket)
+})
+
+test.only(("entfernt produkt bei Menge 0"), () => {
+    const basket = [
+        {
+            name: "Pizza",
+            price: 10,
+            amount: 1
+        }
+    ];
+
+    updateBasketAmount(basket, 0, -1);
+    assert.equal(basket.length,0, 0);
+    console.log(basket)
 })
