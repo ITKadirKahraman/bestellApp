c54c7b5aff6b4ea19fbe03d759f69db297d6319a
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-07-18 23:05:34 +0200

    modify pushBasket

--- tests/pushToBasket.js
+++ tests/pushToBasket.js
@@ -1,9 +1,11 @@
-export function pushToBasket(product) {
-    return basket.push(
+export function pushToBasket(basket, product) {
+    basket.push(
         {
             name: product.name,
             price: product.price,
             amount: product.amount
         }
     );
+
+    return basket;
 }
