dabf9a4e1eb66d538ea5fffee1db212d6d9a324d
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-07-18 23:01:19 +0200

    logic for counter unit test

---
+++ tests/counter.js
@@ -0,0 +1,9 @@
+export function updateBasketAmount(basket, index, value) {
+    basket[index].amount += value;
+
+    if (basket[index].amount <= 0) {
+        basket.splice(index, 1);
+    }
+
+    return basket[index].amount;
+}
